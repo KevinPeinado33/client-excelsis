0551cc41516ff2f8e66d0a01e5c3de98641edc38
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-05-28 17:01:04 -0500

    navbar y banner construido

--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import Header from '../components/Header/Header';
+import Banner from '../components/Banner';
+
 const Principal = () => {
-    return(
-        <h1>Principal ...</h1>
+    return (
+        <>
+            <Header />
+            <Banner />
+            <div className="container marketing">
+                <h1>Principal ...</h1>
+            </div>
+        </>
     )
 }
 
