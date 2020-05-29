9788a1599725295188ecc81c16cf491487add03e
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-05-29 10:46:55 -0500

    restablenciando valores

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Principal from './pages/Principal';
 
 function App() {
   return (
-    <div className="App">
-      <Principal />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Principal} />
+        <Route exact component={() => {
+          return (
+            <h1>Error 404</h1>
+          )
+        }} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import Header from '../components/Header/Header';
-import Banner from '../components/Banner';
-
 const Principal = () => {
     return (
         <>
-            <Header />
-            <Banner />
-            <div className="container marketing">
                 <h1>Principal ...</h1>
-            </div>
         </>
     )
 }
